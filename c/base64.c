493357b56b35a898ae372d1e3570807bbb82464f
Steven Fitzgerald <steve@my.csun.edu>
2024-01-04 15:17:55 -0800

    prepping for decode

--- c/base64.c
+++ c/base64.c
@@ -68,8 +68,10 @@ int encode (byte * in_buff, byte * out_buff) {
 
      // process three bytes at a time
      // but don't process the last row, which has 1, 2, or 3 byte
-     for (in_count=0 ; in_count + 3 < num; in_count += 3) {
-        // load three bytes
+     int tmp;
+     for (in_count=0 ; tmp=in_count + 3, tmp < num; in_count += 3) {
+
+        // load 24 bits, three bytes
         b0 = in_buff[in_count + 0];
         b1 = in_buff[in_count + 1];
         b2 = in_buff[in_count + 2];
@@ -100,9 +102,9 @@ int encode (byte * in_buff, byte * out_buff) {
      }
 
      // Handle the last row that might need padding
+     // This is a bug.. if we have a large file..
+     // I.e., always go into this section
      if (size_in_buff != num) {
-       // This is the last row 
-       fprintf(stderr, "\n\n%d, %d\n", size_in_buff, num);
        b0 = in_buff[in_count + 0];
 
        bytes = num - in_count;
@@ -132,6 +134,8 @@ int encode (byte * in_buff, byte * out_buff) {
        // map bytes
        map(c0);
        map(c1);
+       // map(c2);
+       // map(c3);
 
        // store bytes
        out_buff[out_count + 0] = c0;
@@ -155,7 +159,130 @@ int encode (byte * in_buff, byte * out_buff) {
        }
        out_count = out_count + 4;
      }
-     fprintf(stderr, "\n%d, %d\n", in_count, out_count);
+     write(1, (void *) out_buff, out_count);
+   } while (1==1);
+   return 0;
+}
+
+
+
+
+#define unmap(index)                       \
+   switch ( index ) {                      \
+     case 0 ... 25  : index +=   0 + 'A' ; \
+                      break;               \
+     case 26 ... 51 : index += -26 + 'a';  \
+                      break;               \
+     case 52 ... 61 : index += -52 + '0';  \
+                      break;               \
+     case 62        : index = '+';         \
+                      break;               \
+     case 63        : index = '/';         \
+                      break;               \
+   }
+
+
+
+
+int decode (byte * in_buff, byte * out_buff) {
+  int b0;  // The three output bytes
+  int b1;
+  int b2;
+  int c0;  // The four input characters
+  int c1;
+  int c2;
+  int c3;
+
+  int num;       // The number of bytes to process
+  int in_count;  // The input byte number being processed
+  int out_count; // The output byte number being processed
+  int bytes;     // The number of remaining bytes;  num % 3
+
+  do {
+    in_count  = 0;
+    out_count = 0;
+    num = read(0, (void *) in_buff, size_in_buff);
+    if (num == 0) break;
+
+    // process four bytes at a time
+    // but don't process the last row
+    int tmp;
+    for (in_count=0 ; tmp=in_count + 4, tmp < num; in_count += 4) {
+
+      // load 4 bytes
+      c0 = in_buff[in_count + 0];
+      c1 = in_buff[in_count + 1];
+      c2 = in_buff[in_count + 2];
+      c3 = in_buff[in_count + 3];
+
+      // unmap bytes
+      unmap(c0);
+      unmap(c1);
+      unmap(c2);
+      unmap(c3);
+
+      // 
+      // merge bytes
+      b0 =  (c0 << 18) | (c1 << 12) | (c2 << 6) | c3;
+
+      // chop bytes
+      b0 = (b0 >> 16);          //  c0 = (b0 >> 16) & 0xFF;
+      b1 = (b0 >> 8) & 0xFF;    //  c1 = (b0 >>  8) & 0xFF;
+      b3 = b0 & 0xFF;           //  c2 = (b0 >>  0) & 0xFF;
+
+      // store bytes
+      out_buff[out_count + 0] = b0;
+      out_buff[out_count + 1] = b1;
+      out_buff[out_count + 2] = b2;
+
+      out_count = out_count + 3;
+    }
+
+    {
+      // Handle the last row that might need padding
+
+      // load 4 bytes
+      c0 = in_buff[in_count + 0];
+      c1 = in_buff[in_count + 1];
+      c2 = in_buff[in_count + 2];
+      c3 = in_buff[in_count + 3];
+
+      bytes = 3;
+      // unmap bytes
+      unmap(c0);
+      unmap(c1);
+
+      if (c3 != '=') {
+        unmap(c3);
+      else
+        c3 = 0;
+        bytes = 1;
+      }
+      if (c2 != '=') {
+        unmap(c2);
+      else
+        c2 = 0;
+        bytes = 2;
+      }
+      hexad : group of six
+      octad : group of eight
+
+      // merge bytes
+      b0 =  (c0 << 18) | (c1 << 12) | (c2 << 6) | c3;
+
+      // chop bytes
+      b0 = (b0 >> 16);          //  c0 = (b0 >> 16) & 0xFF;
+      b1 = (b0 >> 8) & 0xFF;    //  c1 = (b0 >>  8) & 0xFF;
+      b3 = b0 & 0xFF;           //  c2 = (b0 >>  0) & 0xFF;
+
+      // store bytes
+      out_buff[out_count + 0] = b0;
+      out_buff[out_count + 1] = b1;
+      out_buff[out_count + 2] = b2;
+
+      // Here we need to print out only the correct number of chars
+      out_count = out_count + bytes;
+     }
      write(1, (void *) out_buff, out_count);
    } while (1==1);
    return 0;
@@ -169,4 +296,6 @@ int main() {
   printf("\n");
   return 0;
 }
+
+
 
